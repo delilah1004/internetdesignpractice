62a19d9fdfbd560f9de6197722eb728d97938e93
Delilah <pskdy8414@gmail.com>
2018-09-17 17:28:06 +0900

    first commit2

--- app/src/main/java/com/cookandroid/helloandroid/MainActivity.java
+++ app/src/main/java/com/cookandroid/helloandroid/MainActivity.java
@@ -2,6 +2,9 @@ package com.cookandroid.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setIcon(R.drawable.ic_launcher_foreground);
+
+        Button bt1 = (Button) findViewById(R.id.button1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "김성민바보", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
