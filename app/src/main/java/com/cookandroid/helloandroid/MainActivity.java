ff3405f36a810386630e722c447c510b6616cef6
Delilah <pskdy8414@gmail.com>
2018-09-17 17:15:46 +0900

    first commit

---
+++ app/src/main/java/com/cookandroid/helloandroid/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cookandroid.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher_foreground);
+    }
+}
